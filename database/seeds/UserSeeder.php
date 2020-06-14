fbf8445bed3500afb69740e6f7ea9f527cb56a83
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2020-06-14 17:04:14 +0600

    Change elements in UserSeeder again

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,7 +12,13 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'id' => 1,
+            'name' => 'Amin',
+            'email' => 'amindiyass@gmail.com',
+            'password' => Hash::make('helloWorld'),
+            'balance' => 10000,
+            'created_at' => now(),
+        ]);
+        DB::table('users')->insert([
             'name' => 'helloWorld',
             'email' => 'helloWorld@gmail.com',
             'password' => Hash::make('helloWorld'),
@@ -20,7 +26,6 @@ class UserSeeder extends Seeder
             'created_at' => now(),
         ]);
         DB::table('users')->insert([
-            'id' => 1,
             'name' => 'darkNet',
             'email' => 'darkNet@gmail.com',
             'password' => Hash::make('helloWorld'),
